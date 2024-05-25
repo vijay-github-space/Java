3531dd06860d5a0d555603ff31513cc20b5642cd
Vijaykumar <150887604+vijay-github-space@users.noreply.github.com>
2024-05-26 00:19:28 +0530

    Day 1

---
+++ chapter1.java
@@ -0,0 +1,19 @@
+package vijayspace;
+
+public class chapter1 
+{
+	public static void main(String[] args) {
+		
+	
+int a=10;
+int b=5;
+System.out.println(a*b);
+	}
+
+public class chapter2
+{
+	public static void main(String[] args) {
+		
+	}
+}
+}
