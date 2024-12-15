bfccb4e318850dcf05bd3cdebb9f4e9ad04fd0fb
vijay <mr.vijaymailbox@gmail.com>
2024-12-15 23:11:42 +0530

    add problems

---
+++ src/problems/Problems_practice.java
@@ -0,0 +1,28 @@
+package problems;
+
+public class Problems_practice {
+
+	public static void main(String[] args) {
+		
+//		Input Format
+//
+//		There is no input for this challenge.
+//
+//		Output Format
+//
+//		You must print two lines of output:
+//
+//		Print Hello, World. on the first line.
+//		Print Hello, Java. on the second line.
+//		Sample Output
+//
+//		Hello, World.
+//		Hello, Java.
+		 
+		System.out.println("Hello, World.");
+		System.out.println("Hello, Java.");
+		
+
+	}
+
+}
